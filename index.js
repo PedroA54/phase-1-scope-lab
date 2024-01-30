c2a6362ac45a220adb7facc914a9a45024f881ca
Pedro Alarcon <pedroa54@icloud.com>
2024-01-29 22:00:24 -0600

    adds complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer(newCustomer) {
+    window.bestCustomer = newCustomer;
+  }
+  function changeLeastFavoriteCustomer() {
+    throw new Error('Assignment to constant variable.');
+  }
